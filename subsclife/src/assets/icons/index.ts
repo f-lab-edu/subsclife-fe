09ae196553b4a99bbc8892f79f03e31dbb5e2082
seongjin <seongjin2427@gmail.com>
2024-09-10 23:57:57 +0900

    [feat] apply vite-plugin-svgr to use svg file easily

---
+++ subsclife/src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as DocsIcon } from "./docs.svg?react";
+export { default as FilledDocsIcon } from "./filled_docs.svg?react";
+export { default as HomeIcon } from "./home.svg?react";
+export { default as FilledHomeIcon } from "./filled_home.svg?react";
+export { default as SearchIcon } from "./search.svg?react";
+export { default as FilledSearchIcon } from "./filled_search.svg?react";
+export { default as Logo } from "./logo.svg?react";
---
+++ subsclife/src/svg.d.ts
@@ -0,0 +1,5 @@
+// svg.d.ts
+declare module "*.svg" {
+  const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default value;
+}
